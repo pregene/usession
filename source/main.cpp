b62c7ad4ea954d3e605b1a3e69a2eab05d8de92b
pregene <pregene@users.noreply.github.com>
2023-07-08 02:39:43 +0900

    cerinfo parse function done
    CACertInfo done

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <list>
 #include <vector>
 
@@ -10,16 +11,46 @@ class CACertInfo
 public:
   CACertInfo() {}
   ~CACerInfo() {}
+
+  vector<string> ParseElement(string el)
+  {
+    vector<string> arrs;
+    istringstream ss(el);
+    string buff;
+    while (getline(ss, buff, '='))
+    {
+      arrs.push_back(buff);
+    } 
+    return arrs;
+  } 
   
   int ParseString(string cert_line)
   {
+    m_certline = cert_line;
+    istringstream ss(cert_line);
+    string el;
+    while (getline(ss, el, '/'))
+    {
+      vector<string> arr=ParseElement(el);
+      if (!arr.empty())
+      {
+        auto i=arr.begin(); ++i;
+        if (*i == "C")  m_nation=*(++i);
+        if (*i == "ST") m_state=*(++i);
+        if (*i == "L")  m_city=*(++i);
+        if (*i == "O")  m_organ=*(++i);
+        if (*i == "OU") m_unit=*(++i);
+        if (*i == "CN") m_email=*(++i);
+      }
+    }
     return 0;
   }
   
+  string m_certline;
   string m_nation; // /C=
   string m_state;  // /ST=
   string m_city;   // /L=
-  string m_organization; // /O=
+  string m_organ; // /O=
   string m_unit;   // /OU=
   string m_email;  // /CN=
 };
